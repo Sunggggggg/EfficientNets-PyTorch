0fa555555762b76940fe855d5b796204b77731bc
sky <sky990319@gmail.com>
2023-07-21 12:13:57 +0900

    Load recaptcha dataset

--- loader.py
+++ loader.py
@@ -1,38 +1,45 @@
-from torch.utils.data import DataLoader
+import numpy as np
+
+from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import ImageFolder
-from torchvision import transforms
+from torchvision import transforms, datasets
 
 
 def get_loaders(root, batch_size, resolution, num_workers=32):
+    # root => data_dir = "./recaptcha-dataset/Large"
+
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
-    train_dataset = ImageFolder(
-        root + "/train",
-        transforms.Compose([
-            transforms.Resize([resolution, resolution]),
-            transforms.RandomResizedCrop(resolution),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
+    data_transforms = transforms.Compose([
+                    transforms.Resize([resolution, resolution]),
+                    transforms.RandomResizedCrop(resolution),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    normalize,
+                    ])
 
-    val_dataset = ImageFolder(
-        root + "/val",
-        transforms.Compose([
-            transforms.Resize([resolution, resolution]),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
+    class_names = ['Bicycle', 'Bridge', 'Bus', 'Car', 
+               'Chimney', 'Crosswalk', 'Hydrant', 
+               'Motorcycle', 'Palm', 'Traffic Light']
 
-    train_loader = DataLoader(train_dataset,
-        batch_size=batch_size, shuffle=True,
-        num_workers=num_workers, pin_memory=True
-    )
+    input_size = 224
 
-    val_loader = DataLoader(val_dataset,
-        batch_size=batch_size, shuffle=False,
-        num_workers=num_workers, pin_memory=True
-    )
+    print("Initializing Datasets and Dataloaders...")
+
+    image_datasets = datasets.ImageFolder(root, data_transforms)
+    num_data = len(image_datasets)
+    indices = np.arange(num_data)
+    np.random.shuffle(indices)  
+
+    train_size = int(num_data*0.8)
+    train_indices = indices[:train_size]
+    val_indices = indices[train_size:]
+    train_set = Subset(image_datasets, train_indices)
+    val_set = Subset(image_datasets, val_indices)
+
+    print('Number of training data:', len(train_set))
+    print('Number of validation data:', len(val_set))
+
+    train_loader = DataLoader(train_set, batch_size = batch_size, shuffle = True, num_workers = num_workers)
+    val_loader = DataLoader(val_set, batch_size = batch_size, shuffle = True, num_workers = num_workers)
     return train_loader, val_loader
